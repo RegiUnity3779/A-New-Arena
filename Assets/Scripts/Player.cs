5f272d82c8a5258f6dab22a21de4f7ddadb93929
RegiUnity3779 <91010699+RegiUnity3779@users.noreply.github.com>
2025-03-07 11:58:44 +0000

    Added MiniMap Mark to Player, Item and NPC

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    public Sprite sprite;
+    public Image miniMapImage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        miniMapImage.sprite = sprite;
     }
 
     // Update is called once per frame
